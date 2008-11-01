7047378e86b7bdd4d893e9d08a8557c68d26ae7c
marcin201 <marcin201>
2008-11-01 16:14:23 +0000

    Added file to repository

--- AutoCorrDlg.py
+++ AutoCorrDlg.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 # AutoCorrDlg.py
-# Dialog for browsing cards in html mode
+# Dialog for editing find replace pairs
 #
 # Author:   Marcin Matuszkiewicz
 #-------------------------------------------------------------------------------
@@ -27,7 +27,37 @@
 #-------------------------------------------------------------------------------
 # CVS information
 # $Source: /cvsroot/pyflashcards/pyFlashCards/AutoCorrDlg.py,v $
-# $Revision: 1.1 $
-# $Date: 2008/11/01 15:53:56 $
+# $Revision: 1.2 $
+# $Date: 2008/11/01 16:14:23 $
 # $Author: marcin201 $
 #-------------------------------------------------------------------------------
+
+import wx
+
+class AutoCorrDlg(wx.Dialog):
+    def __init__(
+            self, parent, ID, autocorr, title='AutoCorr', 
+            size=(600, 500), pos=wx.DefaultPosition, 
+            style=wx.DEFAULT_DIALOG_STYLE):
+        # Instead of calling wx.Dialog.__init__ we precreate the dialog
+        # so we can set an extra style that must be set before
+        # creation, and then we create the GUI dialog using the Create
+        # method.
+        #pre = wx.PreDialog()
+        #pre.SetExtraStyle(wx.DIALOG_EX_CONTEXTHELP)
+        #pre.Create(parent, ID, title, pos, size, style)
+
+        # This next step is the most important, it turns this Python
+        # object into the real wrapper of the dialog (instead of pre)
+        # as far as the wxPython extension is concerned.
+        #self.PostCreate(pre)
+        wx.Dialog.__init__(self, id=ID, name=u'AutoCorrDlg',
+              parent=parent, size=size, style=style, title=title)
+
+        # Now continue with the normal construction of the dialog
+        # contents
+        self.init_ctrls(parent)
+
+
+    def init_ctrls(self, parent):
+        self.ReplaceLabel = wx.StaticTextCtrl(self, 'Replace')
