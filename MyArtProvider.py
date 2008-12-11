a941a472140b3107362fedd92b052f39b8fcc946
marcin201 <marcin201>
2008-12-11 22:10:06 +0000

    Changed custom ART_ ids to strings

--- MyArtProvider.py
+++ MyArtProvider.py
@@ -1,9 +1,9 @@
 import  wx
 import os.path
 
-ART_FORMAT_TEXT_BOLD         = wx.NewId()
-ART_FORMAT_TEXT_ITALIC       = wx.NewId()
-ART_FORMAT_TEXT_UNDERLINE    = wx.NewId()
+ART_FORMAT_TEXT_BOLD         = u'ART_FORMAT_TEXT_BOLD'
+ART_FORMAT_TEXT_ITALIC       = u'ART_FORMAT_TEXT_ITALIC'
+ART_FORMAT_TEXT_UNDERLINE    = u'ART_FORMAT_TEXT_UNDERLINE'
 
 class TangoArtProvider(wx.ArtProvider):
     def __init__(self, runtimepath):
