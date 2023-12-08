d7f1568efeab1037d4dec882eefb5ddcade4738d
= <=>
2023-12-07 18:57:56 -0800

    1. Removed _encode 2. added formating when writing xml file by default

--- XMLDoc.py
+++ XMLDoc.py
@@ -1,14 +1,6 @@
 from xml.dom.minidom import Document, parse, parseString
 import codecs
 
-# Define the encoding to be used
-enc = "utf_8"
-
-def _encode(v):
-    # In Python 3, all strings are unicode by default.
-    # If you need to encode strings for a specific reason, you can use v.encode(enc)
-    return v
-
 class XMLElement:
     def __init__(self, doc, el):
         self.doc = doc
@@ -17,17 +9,17 @@ class XMLElement:
     def __getitem__(self, name):
         a = self.el.getAttributeNode(name)
         if a:
-            return _encode(a.value)
+            return a.value
         return None
 
     def __setitem__(self, name, value):
-        self.el.setAttribute(name, _encode(value))
+        self.el.setAttribute(name, value)
 
     def __delitem__(self, name):
         self.el.removeAttribute(name)
 
     def __str__(self):
-        return _encode(self.doc.toprettyxml())
+        return self.doc.toprettyxml()
 
     def toString(self):
         return self.doc.toxml()
@@ -38,17 +30,17 @@ class XMLElement:
     def get(self, name, default=None):
         a = self.el.getAttributeNode(name)
         if a:
-            return _encode(a.value)
-        return _encode(default)
+            return a.value
+        return default
 
     def add(self, tag, **kwargs):
         el = self.doc.createElement(tag)
         for k, v in kwargs.items():
-            el.setAttribute(k, _encode(str(v)))
+            el.setAttribute(k, str(v))
         return self._inst(self.el.appendChild(el))
 
     def addText(self, data):
-        return self._inst(self.el.appendChild(self.doc.createTextNode(_encode(data))))
+        return self._inst(self.el.appendChild(self.doc.createTextNode(data)))
 
     def addComment(self, data):
         return self._inst(self.el.appendChild(self.doc.createComment(data)))
@@ -58,7 +50,7 @@ class XMLElement:
         for node in self.el.childNodes:
             if node.nodeType == node.TEXT_NODE:
                 rc.append(node.data)
-        return _encode(sep.join(rc))
+        return sep.join(rc)
 
     def getAll(self, tag):
         return list(map(self._inst, self.el.getElementsByTagName(tag)))
@@ -70,16 +62,25 @@ class XMLDocument(XMLElement):
         if tag:
             self.el = self.add(tag, **kwargs).el
 
-    def writexml(self, writer, indent='', addindent='', newl=''):
-        # The writexml method now takes a writer object instead of just a file path.
-        self.doc.writexml(writer, indent, addindent, newl)
+    # def writexml(self, writer, indent='', addindent='', newl=''):
+        #  The writexml method now takes a writer object instead of just a file path.
+        #self.doc.writexml(writer, indent, addindent, newl)
+        #formatted_xml = self.doc.toprettyxml(indent, newl)
+
+    def writexml(self, writer, indent='    ', newl='\n'):
+        try:
+            formatted_xml = self.doc.toprettyxml(indent, newl)
+            writer.write(formatted_xml)
+            print("XML document written successfully.")
+        except Exception as e:
+            print("Error while writing XML document:", e)
 
     def parse(self, d):
         self.doc = self.el = parse(d)
         return self
 
     def parseString(self, d):
-        self.doc = self.el = parseString(_encode(d))
+        self.doc = self.el = parseString(d)
         return self
 
 # Below is an example of how you might use these classes.
@@ -99,6 +100,9 @@ if __name__ == "__main__":
     c.add('back').addText('back 1')
     c.add('chapter').addText('Chapter 1')
     c.add('box').addText('1')
+
+    #pretty_xml_string = doc.toprettyxml(indent="    ")
+    #print(pretty_xml_string)
 
     # Output the document to a file
     with codecs.open('test.xml', 'w', 'utf_8') as f:
